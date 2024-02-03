c71d696db006093b400c6098a6a9b38bd3c1cc73
Novke <novke.brate@gmail.com>
2024-02-03 18:22:00 +0100

    count-possible-wins & evaluate-position

--- src/bot/bot.clj
+++ src/bot/bot.clj
@@ -57,5 +57,68 @@
 
 
 (defn count-possible-wins-horizontal [board player]
-  )
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 6)
+      count
+      (if (check-possible-win-horizontal board i j player)
+        (recur i (inc j) (inc count))
+        (recur i (inc j) count))))))
+
+(defn count-possible-wins-vertical [board player]
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> i 3)
+      (recur 0 (inc j) count)
+      (if (>= j 7)
+      count
+      (if (check-possible-win-vertical board i j player)
+        (recur (inc i) j (inc count))
+        (recur (inc i) j count))))))
+
+(defn count-possible-wins-main-diagonal [board player]
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 3)
+        count
+        (if (check-possible-win-main-diagonal board i j player)
+          (recur i (inc j) (inc count))
+          (recur i (inc j) count))))))
+
+(defn count-possible-wins-secondary-diagonal [board player]
+  (loop [i 3
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 6)
+        count
+        (if (check-possible-win-secondary-diagonal board i j player)
+          (recur i (inc j) (inc count))
+          (recur i (inc j) count))))))
+
+(defn count-possible-wins [board player]
+  (+ (count-possible-wins-horizontal board player)
+     (count-possible-wins-vertical board player)
+     (count-possible-wins-main-diagonal board player)
+     (count-possible-wins-secondary-diagonal board player)))
+
+(defn evaluate-position [board]
+  (if (board/check-win-global board 2)
+    -1000
+    (if (board/check-win-global board 1)
+      1000
+      (if (board/board-full? board)
+        0
+        (- (count-possible-wins board 1) (count-possible-wins board 2))))))
+
+
+
 
