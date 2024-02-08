0e0a6c9ae7fe768479034cafdde618f94a519410
Novke <novke.brate@gmail.com>
2024-02-08 17:22:31 +0100

    count uninterrupted twos in a row

--- src/bot/bot.clj
+++ src/bot/bot.clj
@@ -107,11 +107,159 @@
           (recur i (inc j) (inc count))
           (recur i (inc j) count))))))
 
+;(defn count-possible-wins [board player]
+;  (+ (count-possible-wins-horizontal board player)
+;     (count-possible-wins-vertical board player)
+;     (count-possible-wins-main-diagonal board player)
+;     (count-possible-wins-secondary-diagonal board player)))
 (defn count-possible-wins [board player]
-  (+ (count-possible-wins-horizontal board player)
-     (count-possible-wins-vertical board player)
-     (count-possible-wins-main-diagonal board player)
-     (count-possible-wins-secondary-diagonal board player)))
+  (let [rez (+ (count-possible-wins-horizontal board player)
+               (count-possible-wins-vertical board player)
+               (count-possible-wins-main-diagonal board player)
+               (count-possible-wins-secondary-diagonal board player))]
+    (println "Possible wins player " player " : " rez)
+    rez))
+
+(defn check-twos-in-a-row-horizontal [board row col player]
+  (if (> col 3)
+    false
+    (or
+      (and
+        (occupied-by? board row col player)
+        (occupied-by? board row (+ col 1) player)
+        (unoccupied? board row (+ col 2))
+        (unoccupied? board row (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (occupied-by? board row (+ col 1) player)
+        (occupied-by? board row (+ col 2) player)
+        (unoccupied? board row (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (unoccupied? board row (inc col))
+        (occupied-by? board row (+ col 2) player)
+        (occupied-by? board row (+ col 3) player)))))
+
+(defn check-twos-in-a-row-vertical [board row col player]
+  (if (> row 2)
+    false
+    (or
+      (and
+        (occupied-by? board row col player)
+        (occupied-by? board (+ row 1) col player)
+        (unoccupied? board (+ row 2) col)
+        (unoccupied? board (+ row 3) col))
+      (and
+        (unoccupied? board row col)
+        (occupied-by? board (+ row 1) col player)
+        (occupied-by? board (+ row 2) col player)
+        (unoccupied? board (+ row 3) col))
+      (and
+        (unoccupied? board row col)
+        (unoccupied? board (+ row 1) col)
+        (occupied-by? board (+ row 2) col player)
+        (occupied-by? board (+ row 3) col player)))))
+
+(defn check-twos-in-a-row-main-diagonal [board row col player]
+  (if (or (> row 2) (> col 3))
+    false
+    (or
+      (and
+        (occupied-by? board row col player)
+        (occupied-by? board (+ row 1) (+ col 1) player)
+        (unoccupied? board (+ row 2) (+ col 2))
+        (unoccupied? board (+ row 3) (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (occupied-by? board (+ row 1) (+ col 1) player)
+        (occupied-by? board (+ row 2) (+ col 2) player)
+        (unoccupied? board (+ row 3) (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (unoccupied? board (+ row 1) (+ col 1))
+        (occupied-by? board (+ row 2) (+ col 2) player)
+        (occupied-by? board (+ row 3) (+ col 3) player)))))
+
+(defn check-twos-in-a-row-secondary-diagonal [board row col player]
+  (if (or (< row 3) (> col 3))
+    false
+    (or
+      (and
+        (occupied-by? board row col player)
+        (occupied-by? board (- row 1) (+ col 1) player)
+        (unoccupied? board (- row 2) (+ col 2))
+        (unoccupied? board (- row 3) (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (occupied-by? board (- row 1) (+ col 1) player)
+        (occupied-by? board (- row 2) (+ col 2) player)
+        (unoccupied? board (- row 3) (+ col 3)))
+      (and
+        (unoccupied? board row col)
+        (unoccupied? board (- row 1) (+ col 1))
+        (occupied-by? board (- row 2) (+ col 2) player)
+        (occupied-by? board (- row 3) (+ col 3) player)))))
+(defn count-uninterrupted-twos-in-a-row-horizontal [board player]
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 6)
+        count
+        (if (check-twos-in-a-row-horizontal board i j player)
+          (recur i (inc j) (inc count))
+          (recur i (inc j) count))))))
+
+(defn count-uninterrupted-twos-in-a-row-vertical [board player]
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> i 3)
+      (recur 0 (inc j) count)
+      (if (>= j 7)
+        count
+        (if (check-twos-in-a-row-vertical board i j player)
+          (recur (inc i) j (inc count))
+          (recur (inc i) j count))))))
+
+(defn count-uninterrupted-twos-in-a-row-main-diagonal [board player]
+  (loop [i 0
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 3)
+        count
+        (if (check-twos-in-a-row-main-diagonal board i j player)
+          (recur i (inc j) (inc count))
+          (recur i (inc j) count))))))
+
+(defn count-uninterrupted-twos-in-a-row-secondary-diagonal [board player]
+  (loop [i 3
+         j 0
+         count 0]
+    (if (> j 3)
+      (recur (inc i) 0 count)
+      (if (>= i 6)
+        count
+        (if (check-twos-in-a-row-secondary-diagonal board i j player)
+          (recur i (inc j) (inc count))
+          (recur i (inc j) count))))))
+
+;(defn count-uninterrupted-twos-in-a-row [board player]
+;  (+ (count-uninterrupted-twos-in-a-row-horizontal board player)
+;     (count-uninterrupted-twos-in-a-row-vertical board player)
+;     (count-uninterrupted-twos-in-a-row-main-diagonal board player)
+;     (count-uninterrupted-twos-in-a-row-secondary-diagonal board player)))
+
+(defn count-uninterrupted-twos-in-a-row [board player]
+  (let [rez (+ (count-uninterrupted-twos-in-a-row-horizontal board player)
+     (count-uninterrupted-twos-in-a-row-vertical board player)
+     (count-uninterrupted-twos-in-a-row-main-diagonal board player)
+     (count-uninterrupted-twos-in-a-row-secondary-diagonal board player))]
+    (println "Uninterrupted twos player " player " : " rez)
+    rez))
 
 (defn evaluate-position [board]
   (if (board/check-win-global board 2)
@@ -120,7 +268,9 @@
       1000
       (if (board/board-full? board)
         0
-        (- (count-possible-wins board 1) (count-possible-wins board 2))))))
+        (+
+          (- (count-possible-wins board 1) (count-possible-wins board 2))
+          (- (count-uninterrupted-twos-in-a-row board 1) (count-uninterrupted-twos-in-a-row board 2)))))))
 
 
 
