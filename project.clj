74e317fcdbec4c8fcdaa12d42db4b20b498d2c6a
Novke <novke.brate@gmail.com>
2024-03-15 19:23:33 +0100

    update lein

--- project.clj
+++ project.clj
@@ -1,7 +1,15 @@
 (defproject connect-four-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns connect-four-bot.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [midje "1.9.3"]]
+  :repl-options {:init-ns connect-four-bot.core}
+  :plugins [[lein-cljsbuild "1.1.8"]]
+  :cljsbuild {
+              :builds [{
+                        ; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src/js"]
+                        ; The standard ClojureScript compiler options:
+                        ; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                                   :output-to "helloworld.js"  ; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
